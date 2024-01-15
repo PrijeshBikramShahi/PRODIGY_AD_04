572cf14509972099bbdf4fd575f32d809c23cd3b
rYuma <prijeshshahi0@gmail.com>
2024-01-15 23:35:21 +0545

    logo added

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -10,17 +10,7 @@ class Homepage extends StatefulWidget {
 
 class _HomepageState extends State<Homepage> {
   int oTurn = 0;
-  List<String> xoList = [
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-  ];
+  List<String> xoList = ['', '', '', '', '', '', '', '', ''];
 
   void onXOTap(int index) {
     setState(() {
@@ -35,42 +25,42 @@ class _HomepageState extends State<Homepage> {
             xoList[index] = 'X';
           });
         }
-      checkForWinner();
+        checkForWinner(index);
       }
     });
   }
 
-  void checkForWinner() {
-    for (int i = 0; i < 3; i++) {
-      if (xoList[i][0] != '-' &&
-          xoList[i][0] == xoList[i][1] &&
-          xoList[i][0] == xoList[i][2]) {
-        showWinnerScreen;
-      }
+  void checkForWinner(index) {
+    if (xoList[0] != '' && xoList[0] == xoList[1] && xoList[0] == xoList[2]) {
+      showWinnerScreen();
     }
-    for (int j = 0; j < 3; j++) {
-      if (xoList[0][j] != '-' &&
-          xoList[0][j] == xoList[1][j] &&
-          xoList[1][j] == xoList[2][j]) {
-        showWinnerScreen;
-      }
+    if (xoList[3] != '' && xoList[3] == xoList[4] && xoList[3] == xoList[5]) {
+      showWinnerScreen();
     }
-    if (xoList[0][0] != '-' &&
-        xoList[0][0] == xoList[1][1] &&
-        xoList[1][1] == xoList[2][2]) {
-      showWinnerScreen;
+    if (xoList[6] != '' && xoList[6] == xoList[7] && xoList[6] == xoList[8]) {
+      showWinnerScreen();
     }
-    if (xoList[0][2] != '-' &&
-        xoList[0][2] == xoList[1][1] &&
-        xoList[1][1] == xoList[2][0]) {
-      showWinnerScreen;
+    if (xoList[0] != '' && xoList[0] == xoList[3] && xoList[0] == xoList[6]) {
+      showWinnerScreen();
+    }
+    if (xoList[1] != '' && xoList[1] == xoList[4] && xoList[1] == xoList[7]) {
+      showWinnerScreen();
+    }
+    if (xoList[2] != '' && xoList[2] == xoList[5] && xoList[2] == xoList[8]) {
+      showWinnerScreen();
+    }
+    if (xoList[0] != '' && xoList[0] == xoList[4] && xoList[0] == xoList[8]) {
+      showWinnerScreen();
+    }
+    if (xoList[6] != '' && xoList[6] == xoList[4] && xoList[6] == xoList[2]) {
+      showWinnerScreen();
     }
   }
 
   void showWinnerScreen() {
     showDialog(
       context: context,
-      builder: (context) => AlertDialog(
+      builder: (context) => const AlertDialog(
         title: Text("WINNER!!"),
       ),
     );
@@ -79,23 +69,57 @@ class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.grey[800],
-        body: Padding(
-          padding: const EdgeInsets.only(top:30.0).copyWith(left: 10,right: 10),
-          child: GridView.builder(
-            itemCount: 9,
-            gridDelegate:
-                SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
-            itemBuilder: (context, index) {
-              return TickBox(
-                displayXO: xoList,
-                onTap: () {
-                  onXOTap(index);
-                },
-                currentIndex: index,
-              );
-            },
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Cosmic",
+                style: TextStyle(
+                  fontSize: 40,
+                  color: Colors.white,
+                ),
+              ),
+              Text(
+                "XO",
+                style: TextStyle(
+                  fontSize: 40,
+                  color: Colors.redAccent,
+                ),
+              ),
+            ],
           ),
+        ),
+        backgroundColor: Colors.grey[800],
+        body: Column(
+          children: [
+            SizedBox(
+              height: 500,
+              child: Padding(
+                padding: const EdgeInsets.only(top: 30.0)
+                    .copyWith(left: 10, right: 10),
+                child: GridView.builder(
+                  physics: NeverScrollableScrollPhysics(),
+                  itemCount: 9,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 3),
+                  itemBuilder: (context, index) {
+                    return TickBox(
+                      displayXO: xoList,
+                      onTap: () {
+                        onXOTap(index);
+                      },
+                      currentIndex: index,
+                    );
+                  },
+                ),
+              ),
+            ),
+            Row(
+              children: [Container()],
+            ),
+          ],
         ));
   }
 }
--- lib/utils/box.dart
+++ lib/utils/box.dart
@@ -1,27 +1,32 @@
 import 'package:flutter/material.dart';
 
+// ignore: must_be_immutable
 class TickBox extends StatelessWidget {
   TickBox({
     super.key,
     required this.currentIndex,
-    required this.onTap, required this.displayXO,
+    required this.onTap,
+    required this.displayXO,
   });
+
   VoidCallback onTap;
   final int currentIndex;
   final List<String> displayXO;
+  @override
   Widget build(BuildContext context) {
     return InkWell(
       splashColor: Colors.white.withOpacity(0.2),
       onTap: onTap,
       child: Container(
-        decoration: BoxDecoration(border: Border.all()),
+        decoration: BoxDecoration(
+            border: Border.all(color: Colors.grey.withOpacity(0.2))),
         height: 50,
         child: Center(
           child: Text(
             displayXO[currentIndex],
             style: TextStyle(
               fontSize: 34,
-              color: Colors.white,
+              color: (displayXO[currentIndex]=='X') ? Colors.redAccent : Colors.white,
             ),
           ),
         ),
