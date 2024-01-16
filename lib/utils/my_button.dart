ca4d5dce1d9a8353b0191f798bcb2eebc69f3471
rYuma <prijeshshahi0@gmail.com>
2024-01-16 09:05:44 +0545

    added all

---
+++ lib/utils/my_button.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+// ignore: must_be_immutable
+class MyButton extends StatelessWidget {
+  MyButton({
+    super.key,
+    required this.onTap,
+    required this.icon,
+  });
+
+  VoidCallback onTap;
+  IconData icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      borderRadius: BorderRadius.circular(15),
+      splashColor: Colors.redAccent.withOpacity(
+        0.2,
+      ),
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          border: Border.all(
+            width: 2,
+            color: Colors.grey.withOpacity(0.3),
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Icon(
+            icon,
+            size: 40,
+            color: const Color.fromARGB(255, 160, 40, 40),
+          ),
+        ),
+      ),
+    );
+  }
+}
